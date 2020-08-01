72079a3ad09b4b035a2de1a78f4808c55425655b
David Carlisle <dave_0021@yahoo.com>
2020-08-01 13:20:37 -0500

    updated gitignore

---
+++ client/build/precache-manifest.979a7551b20a3330b47dcf3937bab7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f3e698aacf07fb4a10d582f411cc7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1683bf94b65852520121",
+    "url": "/static/js/2.a03bad0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a03bad0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d4ea0c85531112855c",
+    "url": "/static/js/main.7883da86.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/static/media/logo-bootstrap.83fe61b0.svg"
+  }
+]);
